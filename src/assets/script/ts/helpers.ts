54f4a2c75bd360bc6a109a43490175635f0488f1
Aleksandr Mironenko <alfie.mir@gmail.com>
2020-07-04 17:24:52 +0300

    Набросал реализацию одного рендера одного месяца

--- src/assets/script/ts/helpers.ts
+++ src/assets/script/ts/helpers.ts
@@ -5,3 +5,13 @@ export function createEl(tag: string): HTMLElement {
 export function getDaysInMonth(month: number): number {
   return 33 - new Date(2020, month, 33).getDate();
 }
+
+export function getRightDay(year: number, month: number): number {
+  let numberOfWeekDay = new Date(year, month, 1).getDay() - 1;
+
+  if (numberOfWeekDay === -1) {
+    numberOfWeekDay = 6;
+  }
+
+  return numberOfWeekDay;
+}
--- src/assets/script/ts/index.ts
+++ src/assets/script/ts/index.ts
@@ -1,7 +1,7 @@
 //new Date.getDay(); - получить день (0 - 6. День 6 - это суббота)
 //new Date().getMonth(); - получить месяц (0 - 11. День 11 - это Декабрь)
 
-import { createEl, getDaysInMonth } from './helpers';
+import { createEl, getDaysInMonth, getRightDay } from './helpers';
 
 const days: string[] = ['Mo', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']; // массив дней недели
 const months: string[] = [
@@ -19,24 +19,18 @@ const months: string[] = [
   'December',
 ]; // массив дней недели
 
-/**
- * Функция для получения текущего года
- */
-function getCurrentYear(): number {
-  return new Date().getFullYear();
-}
-
 function createMonth(days: string[]) {
+  const yearContainer = document.getElementById('year');
   const div: HTMLElement = createEl('div');
   const table: HTMLElement = createEl('table');
   const thead: HTMLElement = createEl('thead');
   const tbody: HTMLElement = createEl('tbody');
   const tr: HTMLElement = createEl('tr');
-  const td: HTMLElement = createEl('td');
   const h3: HTMLElement = createEl('h3');
 
   div.classList.add('month');
   h3.classList.add('month-name');
+  div.appendChild(h3);
 
   const currentYear = new Date().getFullYear();
   const startMonth = 0; // Номер месяца (по умолчанию 0 - Январь)
@@ -45,35 +39,93 @@ function createMonth(days: string[]) {
    * Создаю шапку с днями недели
    */
   function createWeekString(days: string[], tr: HTMLElement): void {
-
     days.forEach(day => {
       const th: HTMLElement = createEl('th');
       th.innerHTML = day;
 
       tr.appendChild(th);
     });
+
+    thead.appendChild(tr);
   }
 
   /**
    * Создаю дни в месяце
    */
   function createDays(year: number, month: number): void {
-    const dayOfWeek = new Date(year, month, 1).getDay(); // День недели первого дня месяца
+    const dayOfWeek = getRightDay(year, month); // День недели первого дня месяца
     const totalDays: number = getDaysInMonth(month); // кол-во дней в месяце
-    const tr: HTMLElement = createEl('tr');
+    const tdArray = createTdArray(dayOfWeek, totalDays);
 
-    for (let i = 1; i <= totalDays; i++) {
-      const td: HTMLElement = createEl('td');
+    function createTdArray(dayOfWeek: number, totalDays: number): HTMLElement[] {
+      const arr: HTMLElement[] = [];
+      let day = 0;
+      // 35 ячеек создаются (5 рядов по 7 дней в ряду, пустые скрываются стилями
+      for (let i = 0; i < 35; i++) {
+        const td: HTMLElement = createEl('td');
+        // Проверка на кол-во дней. Заполняются только дни в месяце. А так же только начиная со дня недели
+        if (i >= dayOfWeek && i < totalDays + dayOfWeek) {
+          day = day + 1;
+          td.innerHTML = day.toString();
+        }
 
-      td.innerHTML = i.toString();
-      tr.appendChild(td);
+        arr.push(td);
+      }
+
+      return arr;
     }
 
-    console.log(tr);
+    function createTbody(tbody: HTMLElement): void {
+      let trArray: HTMLElement[] = [];
+
+      tdArray.forEach(td => {
+
+        trArray.push(td);
+
+        if (trArray.length >= 7) {
+          const tr: HTMLElement = createEl('tr');
+          trArray.forEach(td => tr.appendChild(td));
+          tbody.appendChild(tr);
+          trArray = [];
+        }
+      });
+    }
+
+    createTbody(tbody);
   }
 
-  createDays(currentYear, startMonth);
+  /**
+   * Вставляю месяц в календарь
+   */
+  function appendMonth(): void {
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    div.appendChild(table);
+    yearContainer.appendChild(div);
+  }
+
   createWeekString(days, tr);
+  createDays(currentYear, startMonth);
+  appendMonth();
 }
 
 createMonth(days);
+
+/**
+ * В этом классе по умолчанию используется текущий год. Либо можно передать свой, тот
+ * с которого начнется календарь при загрузке страницы
+ */
+class Calendar {
+  currentYear: number;
+
+  constructor(currentYear: number = new Date().getFullYear()) {
+    this.currentYear = currentYear;
+    this.init();
+  }
+
+  init(): void {
+    console.log(this.currentYear);
+  }
+}
+
+const yr = new Calendar();
